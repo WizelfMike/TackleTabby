22c93c0a53311088b7c3750cdbaf137de3916781
WizelfMike <mikeyclarke.com@gmail.com>
2025-03-27 15:39:16 +0100

    Got a start on the fish manager with making the refurbished Combo Manager working for testing again.

--- TackleTabby/Assets/Scripts/Playfield/MatchRemover.cs
+++ TackleTabby/Assets/Scripts/Playfield/MatchRemover.cs
@@ -1,48 +1,63 @@
-﻿    using System.Collections.Generic;
-    using System.Linq;
-    using UnityEngine;
-    using UnityEngine.Events;
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.Events;
 
-    public class MatchRemover : MonoBehaviour
+public class MatchRemover : MonoBehaviour
+{
+    [SerializeField]
+    private MatchMediator MatchMediator;
+    [SerializeField]
+    private FieldBlockPool BlockPool;
+
+    public UnityEvent<Match> OnMatchDestroyed = new();
+    public UnityEvent<int[]> OnRemovedFromColumns = new();
+
+    private MatchMapper _matchMapper = new();
+
+#if UNITY_EDITOR
+    [SerializeField]
+    private BaitDefinition SimulateBait;
+#endif 
+
+    private void Start()
     {
-        [SerializeField]
-        private MatchMediator MatchMediator;
-        [SerializeField]
-        private FieldBlockPool BlockPool;
-
-        public UnityEvent<Match> OnMatchDestroyed = new();
-        public UnityEvent<int[]> OnRemovedFromColumns = new();
-
-        private MatchMapper _matchMapper = new();
-
-        private void Start()
-        {
-            MatchMediator.OnMatchFound.AddListener(OnMatchFound);
-        }
-
-        private void OnMatchFound(ICollection<FieldBlock> fieldBlocks)
-        {
-            Match fieldMatch = _matchMapper.MapFrom(fieldBlocks);
-            int[] columns = CollectColumns(fieldBlocks);
-            
-            foreach (FieldBlock block in fieldBlocks)
-                DestroyBlock(block);
-
-            OnMatchDestroyed.Invoke(fieldMatch);
-            OnRemovedFromColumns.Invoke(columns);
-        }
-
-        private void DestroyBlock(FieldBlock block)
-        {
-            BlockPool.Store(block);
-        }
-
-        private int[] CollectColumns(ICollection<FieldBlock> fieldBlocks)
-        {
-            HashSet<int> columns = new();
-            foreach (FieldBlock block in fieldBlocks)
-                columns.Add(block.HorizontalPosition);
-
-            return columns.ToArray();
-        }
+        MatchMediator.OnMatchFound.AddListener(OnMatchFound);
     }
+
+    private void OnMatchFound(ICollection<FieldBlock> fieldBlocks)
+    {
+        Match fieldMatch = _matchMapper.MapFrom(fieldBlocks);
+        int[] columns = CollectColumns(fieldBlocks);
+            
+        foreach (FieldBlock block in fieldBlocks)
+            DestroyBlock(block);
+
+        OnMatchDestroyed.Invoke(fieldMatch);
+        OnRemovedFromColumns.Invoke(columns);
+    }
+
+    private void DestroyBlock(FieldBlock block)
+    {
+        BlockPool.Store(block);
+    }
+
+    private int[] CollectColumns(ICollection<FieldBlock> fieldBlocks)
+    {
+        HashSet<int> columns = new();
+        foreach (FieldBlock block in fieldBlocks)
+            columns.Add(block.HorizontalPosition);
+
+        return columns.ToArray();
+    }
+
+#if UNITY_EDITOR
+
+    [ContextMenu("Simulate/Bait")]
+    private void SimulateBaitMatch()
+    {
+        OnMatchDestroyed.Invoke(new Match { BaitType = SimulateBait, MatchSize = 3 });
+    }
+
+#endif
+}
