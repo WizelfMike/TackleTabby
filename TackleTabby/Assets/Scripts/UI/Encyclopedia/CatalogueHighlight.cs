99e9b32be36b599f28037194314f2756b241ac95
daan41114 <rendomgamer41114@gmail.com>
2025-04-15 23:53:41 +0200

    improved POC

--- TackleTabby/Assets/Scripts/UI/Encyclopedia/CatalogueHighlight.cs
+++ TackleTabby/Assets/Scripts/UI/Encyclopedia/CatalogueHighlight.cs
@@ -1,21 +1,29 @@
+using System;
 using UnityEngine;
+using UnityEngine.Events;
 
-public class CatalogueHighlight : MonoBehaviour
+public class CatalogueHighlight : MonoBehaviour //, IOverlayMenu
 {
     [SerializeField] 
     private GameObject FieldGrayout;
-    [SerializeField] 
-    private Encyclopedia Encyclopedia;
 
-    [SerializeField] private MenuCommunicator MenuCommunicator;
-    public void EnableHighlight()
+    private bool _hasBeenOpened;
+    
+    //public UnityEvent<IOverlayMenu> OnOpened;
+   // public UnityEvent<IOverlayMenu> OnClosed;
+    
+    public void Open()
     {
+        if (_hasBeenOpened)
+            return;
+        _hasBeenOpened = true;
+        gameObject.SetActive(true);
         FieldGrayout.SetActive(true);
-        
     }
 
-    public void DisableHighlight()
+    public void Close()
     {
-        
+        gameObject.SetActive(false);
+        FieldGrayout.SetActive(false);
     }
 }
