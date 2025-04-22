d288014ff47d5376811b009244e6a478e3787907
MitchelMA <32548@ma-web.nl>
2025-04-22 15:39:13 +0200

    Combo tracking is now done with the indicators through animations

--- TackleTabby/Assets/Scripts/Combos/ComboUI.cs
+++ TackleTabby/Assets/Scripts/Combos/ComboUI.cs
@@ -1,13 +1,21 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ComboUI : MonoBehaviour
 {
+    [Header("Combo Value Setting")]
     [SerializeField]
     private List<ComboSlot> ComboSlots = new();
     [SerializeField]
     private ComboTracker ComboTracker;
+    [Header("Animation")]
+    [SerializeField]
+    private string OpenTriggerName;
+    [SerializeField]
+    private string CloseTriggerName;
 
+    private Queue<Match> _progressQueue = new();
     private List<Match> _progress = new();
 
     private void Start()
@@ -20,18 +28,31 @@ public class ComboUI : MonoBehaviour
 
     private void UpdateComboUI(Match match)
     {
-        if (_progress.Count == 0)
-            ResetComboUI();
-
-        _progress.Add(match);
-        EnableSlot(_progress.Count - 1, true);
-        ComboSlots[_progress.Count - 1].BaitMatchImage.sprite = match.BaitType.BaitSprite;
-        ComboSlots[_progress.Count - 1].BaitMatchSizeText.SetText($"{match.MatchSize}x");
+        // if (_progress.Count == 0)
+        //     ResetComboUI();
+        
+        _progressQueue.Enqueue(match);
+        HandleProgressQueue();
     }
 
     private void OnComboFinished(Combo combo)
     {
+        // Invoke(nameof(OnComboFinishedDelayed), 0.5f);
+        StartCoroutine(OnComboFinishedDelayed());
+    }
+
+    private IEnumerator OnComboFinishedDelayed()
+    {
+        yield return new WaitForSeconds(0.5f);
+        ResetComboUI();
+
+        yield return new WaitForSeconds(0.5f);
         ClearProgress();
+        while (true)
+        {
+            if (!HandleProgressQueue())
+                break;
+        }
     }
 
     private void ClearProgress()
@@ -41,8 +62,20 @@ public class ComboUI : MonoBehaviour
 
     private void EnableSlot(int index, bool enable = true)
     {
-        ComboSlots[index].BaitMatchImage.enabled = enable;
-        ComboSlots[index].BaitMatchSizeText.enabled = enable;
+        ComboSlots[index].BaitAnimator.SetTrigger(enable ? OpenTriggerName : CloseTriggerName);
+    }
+
+    private bool HandleProgressQueue()
+    {
+        if (_progress.Count >= ComboSlots.Count)
+            return false;
+        
+        Match match = _progressQueue.Dequeue();
+        _progress.Add(match);
+        EnableSlot(_progress.Count - 1, true);
+        ComboSlots[_progress.Count - 1].BaitMatchImage.sprite = match.BaitType.BaitSprite;
+        ComboSlots[_progress.Count - 1].BaitMatchSizeText.SetText($"{match.MatchSize}x");
+        return true;
     }
 
     public void ResetComboUI()
