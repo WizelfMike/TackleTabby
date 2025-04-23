300d808dd163bba9539aea02b969610a84a029f6
WizelfMike <mikeyclarke.com@gmail.com>
2025-04-23 15:11:19 +0200

    Settings Menu has been added, with volume controls.

--- TackleTabby/Assets/Scripts/Audio/AudioHandler.cs
+++ TackleTabby/Assets/Scripts/Audio/AudioHandler.cs
@@ -3,12 +3,27 @@ using UnityEngine;
 
 public class AudioHandler : MonoBehaviour
 {
+    [Header("Volume Controls")]
+    [SerializeField]
+    private float MusicVolume = 1f;
+    [SerializeField]
+    private float SFXVolume = 1f;
+    [SerializeField]
+    private float MasterVolume = 2f;
+
+    private FMOD.Studio.Bus _musicBus;
+    private FMOD.Studio.Bus _sfxBus;
+    private FMOD.Studio.Bus _masterBus;
 
     private FMOD.Studio.EventInstance _backgroundMusic;
 
     private void Start()
     {
         FMODUnity.RuntimeManager.WaitForAllSampleLoading();
+
+        _musicBus = FMODUnity.RuntimeManager.GetBus("bus:/Master/Music");
+        _sfxBus = FMODUnity.RuntimeManager.GetBus("bus:/Master/Sound Effects");
+        _masterBus = FMODUnity.RuntimeManager.GetBus("bus:/Master");
 
         _backgroundMusic = FMODUnity.RuntimeManager.CreateInstance("event:/Match3Music");
         _backgroundMusic.start();
@@ -25,4 +40,26 @@ public class AudioHandler : MonoBehaviour
     {
         _backgroundMusic.setParameterByName("Catalogue Open", ParameterValue);
     }
+
+    public void SetMusicVolume(float Volume)
+    {
+        MusicVolume = Volume;
+
+        _musicBus.setVolume(MusicVolume);
+    }
+
+    public void SetSFXVolume(float Volume)
+    {
+        SFXVolume = Volume;
+
+        _sfxBus.setVolume(SFXVolume);
+    }
+
+    public void SetMasterVolume(float Volume)
+    {
+        MasterVolume = Volume;
+
+        _masterBus.setVolume(MasterVolume);
+    }
+
 }
