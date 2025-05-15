c9b8090b016e3f0b9b41c614d5d09504c272bc85
WizelfMike <mikeyclarke.com@gmail.com>
2025-05-15 11:41:36 +0200

    Fixed up some audio issues, ready for the merge

--- TackleTabby/Assets/Scripts/Win Lose Screens/FullReset.cs
+++ TackleTabby/Assets/Scripts/Win Lose Screens/FullReset.cs
@@ -6,9 +6,14 @@ public class FullReset : MonoBehaviour
     [SerializeField]
     private SaveManager SaveManager;
 
+    [SerializeField]
+    private AudioHandler AudioHandler;
+
     public void ResetData()
     {
         SaveManager.ResetProgress();
+
+        AudioHandler.EndMusicPlayback();
 
         Scene currentScene = SceneManager.GetActiveScene();
         SceneManager.LoadScene(currentScene.buildIndex);
